9a557367117ec2c7a09f0988614f0a7a7f8753c5
Everson Esteves <esteveseverson@gmail.com>
2024-10-01 22:01:02 -0300

    button function

--- app/src/main/java/com/example/first_activity/MainActivity.kt
+++ app/src/main/java/com/example/first_activity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.first_activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var cont = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val text_contador = findViewById<TextView>(R.id.textView)
+        val btn_aumentar = findViewById<Button>(R.id.activity_main_btn_aumentar)
+        val btn_diminuir = findViewById<Button>(R.id.activity_main_btn_diminuir)
+
+        btn_aumentar.setOnClickListener{
+            text_contador.text = "${++cont}"
+        }
+
+        btn_diminuir.setOnClickListener{
+            if (cont >= 1) {
+                text_contador.text = "${--cont}"
+            }
+        }
+
     }
 }
